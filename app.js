7736bd3acdb6f8de03b0ea47d93c3445e63632c1
Wafic Lawand <wafic.lawand@outlook.com>
2018-09-06 22:53:03 +0300

    Added signup and signin icons in the header/Added headers and footers to all pages/added login, home, and signup routes/shifted all styles to main.css

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var express = require("express"),
+  
     app = express();
+    app.use(express.static(__dirname+"/public"));
+
+app.get("/",function(req,res){
+
+    res.render("landing.ejs");
+});
+
+app.get("/signup",function(req,res){
+    res.render("signup.ejs");
+});
+
+app.get("/login",function(req,res){
+    res.render("signin.ejs");
+});
+
+
+
+
     app.listen(3000,function(){
         console.log("Servers are running!");
     });

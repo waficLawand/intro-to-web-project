f0eb463e3c53b277d6cb91e2dd93075283426c40
Wafic Lawand <wafic.lawand@outlook.com>
2018-09-07 18:53:44 +0300

    Added a connection to the database

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 var express = require("express"),
-  
-    app = express();
+    app = express(),
+    mySql   = require("mysql");
     app.use(express.static(__dirname+"/public"));
+
+var connection = mySql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'intro-to-web-project-database'
+
+});
+
+connection.getConnection(function(err,connection){
+    if(!err)
+    {
+        console.log("Connected to Database Successfully!");
+    } else {
+        console.log("Failed to connect to database!");
+    }
+
+});
+
 
 app.get("/",function(req,res){
 
